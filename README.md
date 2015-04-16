2f2b2bd33d9c959e469d29bd2537a05433daf990
maxbbender <maxbbender@gmail.com>
2015-04-16 07:17:17 -0700

    Fixed the time script to use EPOCH time as if you are registering at 7:15 or any non incremental hour it wouldn't work

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Detect vs. Date
 ------------
 There are two scripts included. The first (date) is from a script altered by a student at ACU the second is the original from Hopkins @Farist. Use either one, doesn't matter. Just make sure you set it up.
 
+Date Setup 
+------------
+To setup the date script you have to go convert the time you are registering to EPOCH time in milliseconds. You can do that here: http://www.epochconverter.com/#tools ~ Put that time into the variable labled as "registrationEpoch" and you should be all set. MAKE SURE YOU GET THE MILLISECOND VERSION; IF YOU USE THE 'SECONDS' IT WILL NOT WORK AND YOU WILL NOT REGISTER.
+
 Usage
 -----
 Add in the CRN numbers of your classes starting on line 37. Replace the 'XXXXX' with your class CRNs. Load script on the 'Student->Registration' page of 'Banner Self Service' from clicking the Banner link on MyACU. The script will launch upon clicking the 'add or drop classes' option. After clicking on that link, submit your registration code, then go back to menu then go back to 'add or drop classes' and it will start running.
--- farist_registration_sniper_date.js
+++ farist_registration_sniper_date.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name       Registration Sniper
 // @namespace  http://use.i.E.your.homepage/
-// @version    2.5
+// @version    2.61
 // @description  Registers user for classes in under a second
 // @include      https://ssb.banner.marist.edu/*
 // @copyright    2012+, Evan Hopkins, Antony Liang, Bob Nisco
+// @contributor  Max Bender
 // ==/UserScript==
 
 //INSTALLATION: Use either Greasemonkey for Firefox or Tampermonkey for Chrome to run the script.
 //Click new script. Copy and paste the entire text of this document. 
 
-//USAGE: Add in the CRN numbers of your classes starting on line 24. Replace the 'XXXXX'
+//USAGE: Add in the CRN numbers of your classes starting on line 38. Replace the 'XXXXX'
 //with your classes CRN. Load script on the 'Student' tab of the 'Banner Self Service' tab
 //of My Marist. The script will launch upon clicking the 'add or drop classes' option.
 //After clicking on that link, submit your registration code, then go back to menu then go back to
@@ -20,16 +21,25 @@
 //console.log(window.location.href);
 if(window.location.href == 'https://ssb.banner.marist.edu/PROD11G/bwskfreg.P_AltPin'){
     //enter the hour and minute of your registration. NOTE this is a 24 hour set up. So 8PM = 20 (12 + 8).
-    var registrationHour = 7;
+    //var registrationHour = 7; //This is deprecated. Will not work if you are registering at 7:15.
+	//var currentHour = currentDate.getHours(); //Deprecated
+	
+	//Registration Time ~EPOCH~ Setup
+	//Go to http://www.epochconverter.com/#tools and convert the time
+	//that you are registering to EPOCH time. MilliSeconds since Jan 1, 1970.
+	//Put that value into the variable below "registrationEpoch".
+	//MAKE SURE YOU USE THE MILLISECONDS! YOU MUST DO THIS. IF YOU USE SECONDS 
+	//IT WILL NOT WORK!
+	var registrationEpoch = 00000000000000;
     
     //gets the current system hour and minute
-    var currentDate = new Date();
-    var currentHour = currentDate.getHours();
-        
+    var currentDate = new Date(); 
+	var currentEpoch = currentDate.getTime(); //Gets the current epoch time
+  
     //continuously refreshes the page until the it is time
-    if((currentHour < registrationHour)){
-        //console.log("Current time: " + currentHour);
-        //console.log("Registration time: " + registrationHour);
+    if((currentEpoch < registrationEpoch)){
+        console.log("Current time: " + currentEpoch);
+        console.log("Registration time: " + registrationEpoch);
         location.reload();
         }
     else{
